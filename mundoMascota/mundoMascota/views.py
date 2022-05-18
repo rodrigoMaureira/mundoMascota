7b845164cde40fb3214e86b03a7d190d07cfc7c9
rodrigoMaureira <102636132+rodrigoMaureira@users.noreply.github.com>
2022-05-18 00:52:33 -0400

    Add files via upload

---
+++ mundoMascota/mundoMascota/views.py
@@ -0,0 +1,23 @@
+from cgitb import html
+from django.shortcuts import render, HttpResponse
+
+def home(request):
+
+    return render(request, "home.html")
+
+
+def Tienda(request):
+
+    return render(request, "Tienda.html")
+
+def Blog(request):
+
+    return render(request, "Blog.html")
+
+def Contacto(request):
+
+    return render(request, "Contacto.html")
+
+def base(request):
+
+    return render(request, "base.html")   
